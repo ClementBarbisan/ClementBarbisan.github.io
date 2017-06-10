3dc17e9e2863f3a87a3afc5905badc9109505c63
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 18:25:33 +0200

    update

--- initialize.js
+++ initialize.js
@@ -3,8 +3,11 @@ function openFrame(container, string)
     console.log("click");
     var obj = document.getElementById(string);
     obj.src = string + ".html";
-    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-    obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
-    container.style.height = obj.style.height;
-    container.style.width = obj.style.width;
+    setTimeout(function()
+    {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+        container.style.height = obj.style.height;
+        container.style.width = obj.style.width;
+    }, 500);
 }

fee7128026a583428b32b39a1c5f5b8f384c9173
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 20:07:49 +0200

    update

--- initialize.js
+++ initialize.js
@@ -2,9 +2,9 @@ var oldValue = null;
 var oldHeight = 0;
 var oldWidth = 0;
 
-window.onload(function() {
+window.onload = function() {
     document.getElementById("keyStrikeEvent").addEventListener("click", openFrame("keyStrikeEvent", "keyStrike"));
-})
+};
 
 function openFrame(container, string)
 {

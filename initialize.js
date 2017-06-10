35f54074ec1e4246e0f6b4c41d32445891b0833d
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 17:00:07 +0200

    update

---
+++ initialize.js
@@ -0,0 +1,18 @@
+document.getElementById( "keyStrike" ).onclick = function() {
+    Console.Log("click!");
+};
+document.getElementById( "formes" ).onclick = function() {
+    Console.Log("click!");
+};
+document.getElementById( "installations" ).onclick = function() {
+    Console.Log("click!");
+};
+document.getElementById( "particleSystem" ).onclick = function() {
+    Console.Log("click!");
+};
+document.getElementById( "dialogue" ).onclick = function() {
+    Console.Log("click!");
+};
+document.getElementById( "genetype" ).onclick = function() {
+    Console.Log("click!");
+};

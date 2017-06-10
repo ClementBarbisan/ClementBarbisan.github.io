996adca25b2c8b40ab8eb70954b9492202d64f44
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 18:21:48 +0200

    update

--- initialize.js
+++ initialize.js
@@ -1,7 +1,10 @@
-function openFrame(obj, string)
+function openFrame(container, string)
 {
     console.log("click");
+    var obj = document.getElementById(string);
     obj.src = string + ".html";
     obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
     obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+    container.style.height = obj.style.height;
+    container.style.width = obj.style.width;
 }

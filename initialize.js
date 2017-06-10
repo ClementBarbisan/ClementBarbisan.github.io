5c6d7f161ba1681719b193c328f1bfe3c677baba
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 17:04:33 +0200

    update

--- initialize.js
+++ initialize.js
@@ -1,18 +1,20 @@
-document.getElementById( "keyStrike" ).onclick = function() {
-    Console.Log("click!");
-};
-document.getElementById( "formes" ).onclick = function() {
-    Console.Log("click!");
-};
-document.getElementById( "installations" ).onclick = function() {
-    Console.Log("click!");
-};
-document.getElementById( "particleSystem" ).onclick = function() {
-    Console.Log("click!");
-};
-document.getElementById( "dialogue" ).onclick = function() {
-    Console.Log("click!");
-};
-document.getElementById( "genetype" ).onclick = function() {
-    Console.Log("click!");
-};
+window.onload = function() {
+    document.getElementById("keyStrike").onclick = function () {
+        Console.Log("click!");
+    };
+    document.getElementById("formes").onclick = function () {
+        Console.Log("click!");
+    };
+    document.getElementById("installations").onclick = function () {
+        Console.Log("click!");
+    };
+    document.getElementById("particleSystem").onclick = function () {
+        Console.Log("click!");
+    };
+    document.getElementById("dialogue").onclick = function () {
+        Console.Log("click!");
+    };
+    document.getElementById("genetype").onclick = function () {
+        Console.Log("click!");
+    };
+}

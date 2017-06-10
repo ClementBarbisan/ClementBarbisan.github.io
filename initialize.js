995f810d3c41e16da7ea06be508b75b93340acdb
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 18:41:13 +0200

    update

--- initialize.js
+++ initialize.js
@@ -1,6 +1,12 @@
+var oldValue = null;
+
+
 function openFrame(container, string)
 {
-    console.log("click");
+    if (oldValue != null)
+        document.getElementById(oldValue).style.display = 'block';
+    oldValue = string + "Preview";
+    document.getElementById(oldValue).style.display = 'none';
     var obj = document.getElementById(string);
     obj.src = string + ".html";
     setTimeout(function()
@@ -9,5 +15,21 @@ function openFrame(container, string)
         obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
         container.style.height = obj.style.height;
         container.style.width = obj.style.width;
+    }, 1000);
+}
+
+function openIFrame(obj, string)
+{
+    if (oldValue != null)
+        document.getElementById(oldValue).style.display = 'block';
+    oldValue = string + "Preview";
+    document.getElementById(oldValue).style.display = 'none';
+    obj.src = string + ".html";
+    setTimeout(function()
+    {
+        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+        obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+        obj.parent.style.height = obj.style.height;
+        obj.parent.style.width = obj.style.width;
     }, 1000);
 }

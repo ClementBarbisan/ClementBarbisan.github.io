714ccb440e9d0067956cdfc37498bc0513bc51a0
Clement Barbisan <c.barbisan@free.fr>
2017-06-10 19:57:16 +0200

    update

--- initialize.js
+++ initialize.js
@@ -1,15 +1,15 @@
-var oldValue = null;
-var oldHeight = 0;
-var oldWidth = 0;
+// var oldValue = null;
+// var oldHeight = 0;
+// var oldWidth = 0;
 
 function openFrame(container, string)
 {
-    if (oldValue !== null) {
-        document.getElementById(oldValue).src = oldValue+"Preview.html";
-        document.getElementById(oldValue).style.height = oldHeight;
-        document.getElementById(oldValue).style.width = oldWidth;
-    }
-    oldValue = string;
+    // if (oldValue !== null) {
+    //     document.getElementById(oldValue).src = oldValue+"Preview.html";
+    //     document.getElementById(oldValue).style.height = oldHeight;
+    //     document.getElementById(oldValue).style.width = oldWidth;
+    // }
+    // oldValue = string;
     var obj = document.getElementById(string);
     console.log("click");
     obj.src = string + ".html";
@@ -22,18 +22,18 @@ function openFrame(container, string)
     }, 1000);
 }
 
-function openIFrame(obj, string)
-{
-    if (oldValue !== null)
-        document.getElementById(oldValue).style.display = 'block';
-    oldValue = string + "Preview";
-    document.getElementById(oldValue).style.display = 'none';
-    obj.src = string + ".html";
-    setTimeout(function()
-    {
-        obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-        obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
-        obj.parent.style.height = obj.style.height;
-        obj.parent.style.width = obj.style.width;
-    }, 1000);
-}
+// function openIFrame(obj, string)
+// {
+//     if (oldValue !== null)
+//         document.getElementById(oldValue).style.display = 'block';
+//     oldValue = string + "Preview";
+//     document.getElementById(oldValue).style.display = 'none';
+//     obj.src = string + ".html";
+//     setTimeout(function()
+//     {
+//         obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+//         obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
+//         obj.parent.style.height = obj.style.height;
+//         obj.parent.style.width = obj.style.width;
+//     }, 1000);
+// }
